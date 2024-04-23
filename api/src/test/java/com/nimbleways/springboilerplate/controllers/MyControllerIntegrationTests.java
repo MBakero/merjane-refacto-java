f1b455c75c64e8d4d25a7d3bc8371a1360829ad3
MAIMOUN Bakero <bakero.maimoun@gmail.com>
2024-04-23 05:44:43 +0100

    add units tests & integration test

--- api/src/test/java/com/nimbleways/springboilerplate/controllers/MyControllerIntegrationTests.java
+++ api/src/test/java/com/nimbleways/springboilerplate/controllers/MyControllerIntegrationTests.java
@@ -1,11 +1,16 @@
 package com.nimbleways.springboilerplate.controllers;
 
+import com.nimbleways.springboilerplate.dto.product.UnfulfilledOrder;
 import com.nimbleways.springboilerplate.entities.Order;
 import com.nimbleways.springboilerplate.entities.Product;
+import com.nimbleways.springboilerplate.enums.ProductType;
 import com.nimbleways.springboilerplate.repositories.OrderRepository;
 import com.nimbleways.springboilerplate.repositories.ProductRepository;
 import com.nimbleways.springboilerplate.services.implementations.NotificationService;
+import com.nimbleways.springboilerplate.services.implementations.OrderService;
 import com.nimbleways.springboilerplate.utils.Annotations.SetupDatabase;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -33,49 +38,99 @@ import java.util.Set;
 @SpringBootTest
 @AutoConfigureMockMvc
 public class MyControllerIntegrationTests {
-        @Autowired
-        private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-        @MockBean
-        private NotificationService notificationService;
+    @MockBean
+    private NotificationService notificationService;
 
-        @Autowired
-        private OrderRepository orderRepository;
+    @Autowired
+    private OrderRepository orderRepository;
 
-        @Autowired
-        private ProductRepository productRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
-        @Test
-        public void processOrderShouldReturn() throws Exception {
-                List<Product> allProducts = createProducts();
-                Set<Product> orderItems = new HashSet<Product>(allProducts);
-                Order order = createOrder(orderItems);
-                productRepository.saveAll(allProducts);
-                order = orderRepository.save(order);
-                mockMvc.perform(post("/orders/{orderId}/processOrder", order.getId())
-                                .contentType("application/json"))
-                                .andExpect(status().isOk());
-                Order resultOrder = orderRepository.findById(order.getId()).get();
-                assertEquals(resultOrder.getId(), order.getId());
-        }
+    @Autowired
+    private OrderService orderService;
 
-        private static Order createOrder(Set<Product> products) {
-                Order order = new Order();
-                order.setItems(products);
-                return order;
-        }
+    private List<Product> products;
 
-        private static List<Product> createProducts() {
-                List<Product> products = new ArrayList<>();
-                products.add(new Product(null, 15, 30, "NORMAL", "USB Cable", null, null, null));
-                products.add(new Product(null, 10, 0, "NORMAL", "USB Dongle", null, null, null));
-                products.add(new Product(null, 15, 30, "EXPIRABLE", "Butter", LocalDate.now().plusDays(26), null,
-                                null));
-                products.add(new Product(null, 90, 6, "EXPIRABLE", "Milk", LocalDate.now().minusDays(2), null, null));
-                products.add(new Product(null, 15, 30, "SEASONAL", "Watermelon", null, LocalDate.now().minusDays(2),
-                                LocalDate.now().plusDays(58)));
-                products.add(new Product(null, 15, 30, "SEASONAL", "Grapes", null, LocalDate.now().plusDays(180),
-                                LocalDate.now().plusDays(240)));
-                return products;
-        }
+    private Order order;
+
+
+    @BeforeEach
+    void init() {
+        List<Product> allProducts = createProducts();
+        Set<Product> orderItems = new HashSet<Product>(allProducts);
+        Order order = createOrder(orderItems);
+        productRepository.saveAll(allProducts);
+        order = orderRepository.save(order);
+
+        this.products = allProducts;
+        this.order = order;
+    }
+
+    @AfterEach
+    void flush() {
+        this.products = null;
+        this.order = null;
+    }
+
+
+    @Test
+    public void processOrderShouldReturn() throws Exception {
+        mockMvc.perform(post("/orders/{orderId}/processOrder", order.getId())
+                        .contentType("application/json"))
+                .andExpect(status().isOk());
+        Order resultOrder = orderRepository.findById(order.getId()).get();
+        assertEquals(resultOrder.getId(), order.getId());
+    }
+
+    @Test
+    public void handleProcessSaleCommand() {
+
+        // WHEN
+        List<UnfulfilledOrder> unfulfilled = orderService.handleProcessSaleCommand(order.getId());
+
+        // THEN
+        assertEquals(4, unfulfilled.size());
+    }
+
+    private static Order createOrder(Set<Product> products) {
+        Order order = new Order();
+        order.setItems(products);
+        return order;
+    }
+
+    private static List<Product> createProducts() {
+        List<Product> products = new ArrayList<>();
+        products.add(new Product(1L, 15, 30, ProductType.NORMAL, "USB Cable", null, null, null));
+        products.add(new Product(2L, 10, 0, ProductType.NORMAL, "USB Dongle", null, null, null));
+
+        products.add(new Product(3L, 15, 30, ProductType.EXPIRABLE, "Butter", null, LocalDate.now().plusDays(26),
+                null));
+        products.add(new Product(4L, 90, 6, ProductType.EXPIRABLE, "Milk", null, LocalDate.now().minusDays(2), null));
+
+        products.add(new Product(5L, 15, 30, ProductType.SEASONAL, "Watermelon", LocalDate.now().minusDays(2),
+                LocalDate.now().plusDays(58), null));
+        products.add(new Product(6L, 15, 30, ProductType.SEASONAL, "Grapes", LocalDate.now().plusDays(180),
+                LocalDate.now().plusDays(240), null));
+        products.add(new Product(7L, 0, 200, ProductType.FLASHSALE, "flashsale not available anymore", LocalDate.now().minusDays(10),
+                LocalDate.now().minusDays(5), 100));
+
+        products.add(new Product(8L, 0, 200, ProductType.FLASHSALE, "flashsale soldout product", LocalDate.now().minusDays(10),
+                LocalDate.now().plusDays(5), 0));
+
+        products.add(new Product(9L, 10, 35, ProductType.FLASHSALE, "normal flashsale product", LocalDate.now().minusDays(5),
+                LocalDate.now().plusDays(10), 20));
+
+        products.add(new Product(10L, 15, 0, ProductType.FLASHSALE, "new supply is coming product", LocalDate.now().minusDays(5),
+                LocalDate.now().plusDays(20), 10));
+
+        products.add(new Product(11L, 20, 0, ProductType.FLASHSALE, "offer not available anymore product", LocalDate.now().minusDays(5),
+                LocalDate.now().plusDays(10), 20));
+
+        return products;
+    }
+
 }
--- api/src/test/java/com/nimbleways/springboilerplate/services/implementations/MyUnitTests.java
+++ api/src/test/java/com/nimbleways/springboilerplate/services/implementations/MyUnitTests.java
@@ -1,17 +1,24 @@
 package com.nimbleways.springboilerplate.services.implementations;
 
 import com.nimbleways.springboilerplate.entities.Product;
+import com.nimbleways.springboilerplate.enums.ErrorCodes;
+import com.nimbleways.springboilerplate.enums.ProductType;
 import com.nimbleways.springboilerplate.repositories.ProductRepository;
 import com.nimbleways.springboilerplate.utils.Annotations.UnitTest;
 
+import com.nimbleways.springboilerplate.utils.BaseException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDate;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(SpringExtension.class)
 @UnitTest
@@ -24,10 +31,13 @@ public class MyUnitTests {
     @InjectMocks 
     private ProductService productService;
 
+    @InjectMocks
+    private OrderService orderService;
+
     @Test
     public void test() {
         // GIVEN
-        Product product =new Product(null, 15, 0, "NORMAL", "RJ45 Cable", null, null, null);
+        Product product =new Product(null, 15, 0, ProductType.NORMAL, "RJ45 Cable", null, null, null);
 
         Mockito.when(productRepository.save(product)).thenReturn(product);
 
@@ -40,4 +50,75 @@ public class MyUnitTests {
         Mockito.verify(productRepository, Mockito.times(1)).save(product);
         Mockito.verify(notificationService, Mockito.times(1)).sendDelayNotification(product.getLeadTime(), product.getName());
     }
+
+
+    @Test
+    public void testHandleFlashSaleProduct_needSupply() {
+
+        // GIVEN
+        Product product = new Product(10L, 15, 0, ProductType.FLASHSALE, "new supply is coming product", LocalDate.now().minusDays(5),
+                LocalDate.now().plusDays(20), 10);
+
+        // WHEN
+        BaseException be = assertThrows(BaseException.class, () -> productService.handleFlashSaleProduct(product));
+
+        // THEN
+        assertEquals(ErrorCodes.FLASHSALE_NEEDS_SUPPLY.name(), be.getCode());
+        assertEquals(ErrorCodes.FLASHSALE_NEEDS_SUPPLY.message(), be.getMessage());
+        assertEquals(HttpStatus.ACCEPTED, be.getHttpStatus());
+
+        Mockito.verify(notificationService, Mockito.times(1)).sendOutOfStockNotification(product.getName());
+
+    }
+
+    @Test
+    public void testHandleFlashSaleProduct_cantBeAvailable() {
+
+        //GIVEN
+        Product product = new Product(11L, 20, 0, ProductType.FLASHSALE, "offer not available anymore product", LocalDate.now().minusDays(5),
+                LocalDate.now().plusDays(10), 20);
+
+        // WHEN
+        BaseException be = assertThrows(BaseException.class, () -> productService.handleFlashSaleProduct(product));
+
+        // THEN
+        assertEquals(ErrorCodes.FLASHSALE_CANT_BE_AVAILABLE.name(), be.getCode());
+        assertEquals(ErrorCodes.FLASHSALE_CANT_BE_AVAILABLE.message(), be.getMessage());
+        assertEquals(HttpStatus.GONE, be.getHttpStatus());
+    }
+
+    @Test
+    public void testHandleSellFlashSaleProduct_notAvailableAnymore() {
+
+        // GIVEN
+        Product product = new Product(7L, 0, 200, ProductType.FLASHSALE, "flashsale not available anymore", LocalDate.now().minusDays(10),
+                LocalDate.now().minusDays(5), 100);
+
+        // WHEN
+        BaseException be = assertThrows(BaseException.class, () -> orderService.handleSellFlashSaleProduct(product));
+
+        // THEN
+        assertEquals(ErrorCodes.FLASHSALE_NOT_AVAILABLE_ANYMORE.name(), be.getCode());
+        assertEquals(ErrorCodes.FLASHSALE_NOT_AVAILABLE_ANYMORE.message(), be.getMessage());
+        assertEquals(HttpStatus.BAD_REQUEST, be.getHttpStatus());
+
+    }
+
+    @Test
+    public void testHandleSellFlashSaleProduct_soldOut() {
+
+        // GIVEN
+        Product product = new Product(8L, 0, 200, ProductType.FLASHSALE, "flashsale soldout product", LocalDate.now().minusDays(10),
+                LocalDate.now().plusDays(5), 0);
+
+        // WHEN
+        BaseException be = assertThrows(BaseException.class, () -> orderService.handleSellFlashSaleProduct(product));
+
+        // THEN
+        assertEquals(ErrorCodes.FLASHSALE_SOLDOUT.name(), be.getCode());
+        assertEquals(ErrorCodes.FLASHSALE_SOLDOUT.message(), be.getMessage());
+        assertEquals(HttpStatus.BAD_REQUEST, be.getHttpStatus());
+
+    }
+
 }
